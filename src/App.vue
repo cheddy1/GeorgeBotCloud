c3b0ef3b9a837c8c303a791a296f8782b3bdabad
Christian <elcman965@gmail.com>
2023-03-05 22:30:05 -0600

    front end changes

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav> -->
+  <b-navbar class="nav" toggleable="lg" variant="" >
+    <b-navbar-brand style="color: white;" href="#">
+      <img alt="george logo" style="width: 5vh; padding-right: 5px;" src="./assets/logo.png">
+      Welcome to GeorgeBot!
+    </b-navbar-brand>
+  </b-navbar>
   <router-view/>
 </template>
 
 <style>
 #app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   color: whitesmoke;
   height: 100vh;
   background-color: #212121;
 }
 
-nav {
-  padding: 30px;
+.nav {
+  height: 8vh;
+  background-color: #494949;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
