01818a0b9af8820fd1a8f5e1303dfd4b679789b8
Christian <elcman965@gmail.com>
2023-04-13 13:58:33 -0500

    added about page

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,9 @@ const routes = [
     component: HomeView,
   },
   {
-    // path: '/about',
-    // name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import('../views/AboutView.vue'),
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
   },
 ];
 
